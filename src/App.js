bc01636de29d202227c51f4a5cfbfe7231e93822
Greg Babiars <gbabiars@gmail.com>
2017-12-21 12:29:56 -0700

    Refactor to header component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
-      <header className="header">
-        <h1 className="header__title">NBA</h1>
-        <nav className="header__nav">
-          <a href="/teams" className="header__link">
-            Teams
-          </a>
-          <a href="/players" className="header__link">
-            Players
-          </a>
-          <a href="/stats" className="header__link">
-            Stats
-          </a>
-        </nav>
-      </header>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
