54ff4a0c614e99a508cc4a303e4ba0b42377ca3f
Greg Babiars <gbabiars@gmail.com>
2017-12-21 12:25:33 -0700

    Basic header styling with placeholder links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>NBA</h1>
-      </div>
+      <header className="header">
+        <h1 className="header__title">NBA</h1>
+        <nav className="header__nav">
+          <a href="/teams" className="header__link">
+            Teams
+          </a>
+          <a href="/players" className="header__link">
+            Players
+          </a>
+          <a href="/stats" className="header__link">
+            Stats
+          </a>
+        </nav>
+      </header>
     );
   }
 }
